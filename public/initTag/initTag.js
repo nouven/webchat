929538d1c87cad6129dffb1436e2abea5f66ae5d
prokutenb8x <lehoangvudth9@gmail.com>
2021-09-08 16:04:58 +0700

    sua hover

--- public/initTag/initTag.js
+++ public/initTag/initTag.js
@@ -201,6 +201,7 @@ class InitTag {
     const newTag = document.createElement("div");
     newTag.setAttribute("class", "dropdown-item");
     newTag.classList.add("edit__dropdown");
+    newTag.classList.add("friend__search");
     newTag.innerHTML = `
     <div class="user__info">
     <img src=${obj.avatar} class="avatar_onchat" />
