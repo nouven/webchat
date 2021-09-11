e117bbcb510c45adaaecf0fe70c88f11dbc75382
nouien <62942701+nouien@users.noreply.github.com>
2021-09-11 22:38:32 +0700

    delete key.js

--- configs/key.js
+++
@@ -1,6 +0,0 @@
-// clientID: 1078192881477-u43ggpnhnan122l8gbc0r0hod2bekupq.apps.googleusercontent.com;
-// clientsecret: 3U4OeYB2ObTqKM92JsfNw89j;
-module.exports = {
-    googleClientID: '1078192881477-u43ggpnhnan122l8gbc0r0hod2bekupq.apps.googleusercontent.com', 
-    googleClientSecret: '3U4OeYB2ObTqKM92JsfNw89j'
- };
